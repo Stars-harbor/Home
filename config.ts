60c2472cea6e9e5a5dfcb7bbaee34568f5c2f2b6
Stars Harbor <112921458+Stars-harbor@users.noreply.github.com>
2024-07-07 01:55:19 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,31 +2,31 @@ export default {
 
     // 👇页面内容相关配置
     // 名字
-    Name: "MiaoMint",
+    Name: "❖星港◎Star☆",
 
     // 头像
-    Avatar: "https://q1.qlogo.cn/g?b=qq&k=G1ibIlnicR8Ij4CZO3BF6cVA&s=640",
+    Avatar: "https://bu.dusays.com/2023/09/27/6513053bebdb2.webp",
 
     // 介绍
-    Desc: "👋 Hi! 这里是 MiaoMint 的主页",
+    Desc: "👋 Hi! 欢迎来到 ❖星港◎Star☆ 的主页",
 
     // Github 用户名
-    GithubUsername: "MiaoMint",
+    GithubUsername: "Stars-harbor",
 
     // 博客RSS订阅
-    BlogRSS: "https://blog.0u0.ren/feed/",
+    BlogRSS: "https://blog.starsharbor.com/atom.xml",
 
     // 博客链接
-    BlogUrl: "https://blog.0u0.ren",
+    BlogUrl: "https://blog.starsharbor.com",
 
     // 油管链接
-    YoutubeUrl: "https://www.youtube.com/channel/UCEDJlXVGVHClXL_ihWsAfKw",
+    YoutubeUrl: "",
 
     // 推特链接
-    TwitterUrl: "https://twitter.com/MiaoMint_",
+    TwitterUrl: "",
 
     // B站UID
-    BilibiliUid: "52625050",
+    BilibiliUid: "345815762",
 
     // 默认 /home.md 
     // null 则不显示
@@ -36,18 +36,18 @@ export default {
     // 网页信息
     SiteHead: {
         // 站点标题
-        Title: "MiaoMint 的主页",
+        Title: "❖星港◎Star☆ 的主页",
 
         // 站点图标
-        Favicon: "https://q1.qlogo.cn/g?b=qq&k=G1ibIlnicR8Ij4CZO3BF6cVA&s=640",
+        Favicon: "https://bu.dusays.com/2023/09/27/6513053d49e3e.webp",
 
         // 站点关键词 多个使用 “,” 分割
-        KeyWords: "MiaoMint,Erhecy,喵薄荷,主页,g8ink,短链接,博客",
+        KeyWords: "❖星港◎Star☆,主页",
 
         // 站点介绍
-        Desc: "👋 Hi! 这里是 MiaoMint 的主页",
+        Desc: "👋 Hi! 这里是 ❖星港◎Star☆ 的主页",
     },
 
-    SiteBackground: "url(https://img.truimo.com/2022/807b270630a51.jpg) center/cover",
+    SiteBackground: "url(https://bu.dusays.com/2023/10/02/651a4d79bb412.jpg) center/cover",
 
 }
