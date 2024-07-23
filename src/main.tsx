a2c1a2850365f10ea23e81f972a4483fb03ded18
Stars Harbor <112921458+Stars-harbor@users.noreply.github.com>
2024-07-23 22:17:36 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,12 @@ import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import packageConfig from '../package.json'
 import './index.css'
-import * as serviceWorker from './serviceWorker'
 
 console.log(' %c MiaoHome %c ' + packageConfig.version + ' %c https://github.com/MiaoMint/MiaoHome',
   'background:#35495e ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff',
   'background:#ff9a9a ; padding: 1px; border-radius: 0 3px 3px 0;  color: #fff',
   'background:unset ; padding: 1px; border-radius: 0 3px 3px 0;  color: #fff')
 
-// Register the service worker
-serviceWorker.register();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
